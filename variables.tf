5f7475706adc3baecb5cec9bd3bb0ee9ee26ced7
gio506 <giorgimakarovi@gmail.com>
2026-02-15 22:06:32 +0400

    Add Terraform VPC starter

---
+++ variables.tf
@@ -0,0 +1,38 @@
+variable "aws_region" {
+  description = "AWS region where resources will be created"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "project_name" {
+  description = "Name prefix used for all resources"
+  type        = string
+  default     = "vpc-lite"
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "public_subnet_cidr" {
+  description = "CIDR block for the public subnet"
+  type        = string
+  default     = "10.0.1.0/24"
+}
+
+variable "public_subnet_az" {
+  description = "Availability Zone for the public subnet"
+  type        = string
+  default     = "us-east-1a"
+}
+
+variable "common_tags" {
+  description = "Common tags applied to all resources"
+  type        = map(string)
+  default = {
+    ManagedBy   = "terraform"
+    Environment = "dev"
+  }
+}
