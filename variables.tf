df33b58cb670a77a08455a705a68f63333c77c7d
gio506 <giorgimakarovi@gmail.com>
2026-03-14 21:35:57 +0400

    validate: add cidr input validation for network variables

--- variables.tf
+++ variables.tf
@@ -14,12 +14,22 @@ variable "vpc_cidr" {
   description = "CIDR block for the VPC"
   type        = string
   default     = "10.0.0.0/16"
+
+  validation {
+    condition     = can(cidrhost(var.vpc_cidr, 0))
+    error_message = "vpc_cidr must be a valid IPv4 CIDR block."
+  }
 }
 
 variable "public_subnet_cidr" {
   description = "CIDR block for the public subnet"
   type        = string
   default     = "10.0.1.0/24"
+
+  validation {
+    condition     = can(cidrhost(var.public_subnet_cidr, 0))
+    error_message = "public_subnet_cidr must be a valid IPv4 CIDR block."
+  }
 }
 
 variable "public_subnet_az" {
@@ -32,12 +42,22 @@ variable "ssh_ingress_cidrs" {
   description = "Allowed CIDR ranges for SSH (port 22)"
   type        = list(string)
   default     = ["0.0.0.0/0"]
+
+  validation {
+    condition     = alltrue([for cidr in var.ssh_ingress_cidrs : can(cidrhost(cidr, 0))])
+    error_message = "Each ssh_ingress_cidrs entry must be a valid IPv4 CIDR block."
+  }
 }
 
 variable "http_ingress_cidrs" {
   description = "Allowed CIDR ranges for HTTP (port 80)"
   type        = list(string)
   default     = ["0.0.0.0/0"]
+
+  validation {
+    condition     = alltrue([for cidr in var.http_ingress_cidrs : can(cidrhost(cidr, 0))])
+    error_message = "Each http_ingress_cidrs entry must be a valid IPv4 CIDR block."
+  }
 }
 
 variable "common_tags" {
